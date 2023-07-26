10b43575f1804e6a96805f09d0138dd66750762a
gus6615 <wns6615@gmail.com>
2023-07-26 19:21:21 +0900

    feat: add idle & walk animation

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum PLAYER_STATE
+{
+    IDLE,
+    WALK
+}
+
 public class PlayerCtrl : MonoBehaviour
 {
     private static PlayerCtrl Instance;
@@ -17,6 +23,8 @@ public class PlayerCtrl : MonoBehaviour
     }
 
     private NavMeshAgent agent;
+    private Animator animator;
+    public PLAYER_STATE state;
 
     private bool isCanMove;
 
@@ -29,9 +37,12 @@ public class PlayerCtrl : MonoBehaviour
     void Start()
     {
         agent= GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
 
         agent.updateUpAxis = false;
         agent.updateRotation = false;
+        state = PLAYER_STATE.IDLE;
+
         isCanMove = true;
     }
 
@@ -42,6 +53,30 @@ public class PlayerCtrl : MonoBehaviour
         {
             Vector2 move_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             agent.destination = move_pos;
+        }
+
+        SetState();
+        SetAnimation();
+    }
+
+    private void SetState()
+    {
+        if (agent.remainingDistance < 0.01f)
+            state = PLAYER_STATE.IDLE;
+        else
+            state = PLAYER_STATE.WALK;
+    }
+
+    private void SetAnimation()
+    {
+        switch (state)
+        {
+            case PLAYER_STATE.IDLE:
+                animator.SetBool("isWalk", false);
+                break;
+            case PLAYER_STATE.WALK:
+                animator.SetBool("isWalk", true);
+                break;
         }
     }
 }
