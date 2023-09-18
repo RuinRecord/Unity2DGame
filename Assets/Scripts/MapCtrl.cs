cb8d54a1eee8a2fd476ddb95d5ab0355554a23d3
Cheonnyang <57510872+gus6615@users.noreply.github.com>
2023-09-18 16:37:15 +0900

    Comment: 'MapCtrl' 스크립트 주석 추가

--- Assets/Scripts/MapCtrl.cs
+++ Assets/Scripts/MapCtrl.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// 맵 렌더링에 대한 우선 순위 처리를 위한 클래스이다.
+/// Transform을 기반으로 렌더러를 저장한다. 이때, 아무 렌더러가 없다면 우선 순위 처리 대상에서 제외된다.
+/// </summary>
 public class Render
 {
     public Transform transform;
@@ -17,10 +21,14 @@ public class Render
     }
 }
 
+
+/// <summary>
+/// 맵 관련 처리를 담당하는 컨트롤러 클래스이다.
+/// </summary>
 public class MapCtrl : MonoBehaviour
 {
+    /// <summary> MapCtrl 싱글톤 </summary>
     private static MapCtrl Instance;
-
     public static MapCtrl instance
     {
         set
@@ -31,16 +39,22 @@ public class MapCtrl : MonoBehaviour
         get { return Instance; }
     }
 
+
+    /// <summary> 모든 타일맵 렌더러를 탐색하기 위한 부모 오브젝트 </summary>
     [SerializeField]
     private GameObject tileMapTr;
 
+
+    /// <summary> 렌더러를 포함한 모든 오브젝트를 저장한 리스트 </summary>
     [SerializeField]
     private List<Render> sprites_List;
+
 
     private void Awake()
     {
         instance = this;
     }
+
 
     // Start is called before the first frame update
     void Start()
@@ -56,12 +70,15 @@ public class MapCtrl : MonoBehaviour
             sprites_List.Add(new Render(sprites[i].transform));
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        // 만약 리스트에 렌더러가 있다면 렌더링 우선 순위 처리를 수행
         if (sprites_List.Count > 0)
             SetDepthAllofMapObjects();
     }
+
 
     /// <summary>
     /// 맵에 존재하는 모든 오브젝트에 대해 Z Depth에 따른 우선 순위를 설정하는 함수
@@ -77,6 +94,7 @@ public class MapCtrl : MonoBehaviour
                 return -1;
         });
 
+        // 렌더러 우선순위 지정
         for (int i = 0; i < sprites_List.Count; i++)
         {
             if (sprites_List[i].spriteRenderer != null)
@@ -86,11 +104,21 @@ public class MapCtrl : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// 'transform'을 가진 오브젝트를 sprites_list에 저장하는 함수이다.
+    /// </summary>
+    /// <param name="_transform">저장할 오브젝트 Transform</param>
     public void AddSprite(Transform _transform)
     {
         sprites_List.Add(new Render(_transform));
     }
 
+
+    /// <summary>
+    /// 'render'을 가진 오브젝트를 sprites_list에 삭제하는 함수이다.
+    /// </summary>
+    /// <param name="_transform">삭제할 render</param>
     public void RemoveSprite(Render _render)
     {
         if (!sprites_List.Remove(_render))
